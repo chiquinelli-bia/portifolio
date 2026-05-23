2f7d2e96593ab2ba879ce978c4e63a829152d175
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-05-23 11:10:34 -0300

    feat(Componentes): cria estrutura de pastas e da inicio a criação de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-function App() {
+import { BrowserRouter } from "react-router-dom";
+import { Menu } from "@/components/navBar";
 
+function App() {
   return (
-  )
+    <BrowserRouter>
+      <Menu />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
