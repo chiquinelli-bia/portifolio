e155057bcbbfdd17f1212ea4d01c07f7f02766ce
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-06-10 07:51:01 -0300

    feat(navigationList): adiciona class e data theme aos sub itens

--- src/components/sidebar/navigationList/index.jsx
+++ src/components/sidebar/navigationList/index.jsx
@@ -1,14 +1,13 @@
 import { NavLink } from "react-router-dom";
 import styles from "./navigation.module.css";
 import { IoIosArrowDown } from "react-icons/io";
-
 const NavigationList = ({ itens }) => {
   return (
     <ul className={`${styles.showDropdown} ${styles.menu}`}>
       {itens.map((item) => {
         const Icon = item.icon;
         return (
-          <li key={item.id} className={styles.menuItem}>
+          <li key={item.id} className={styles.menuItem} data-theme="azul">
             <div className={styles.menuHeader}>
               <NavLink
                 to={item.rota}
@@ -17,15 +16,21 @@ const NavigationList = ({ itens }) => {
               `}
               >
                 <Icon size={30} />
-                {/*  onClick={aoDeletar} */}
+                {/*  onClick={} */}
                 {item.texto}
               </NavLink>
               {item.subItem && <IoIosArrowDown size={18} />}
             </div>
             {item.subItem && (
-              <ul>
-                <li key={item.subItem.id}>
-                  <NavLink to={item.subItem.rota}>{item.subItem.texto}</NavLink>
+              <ul className={styles.subList}>
+                <li
+                  key={item.subItem.id}
+                  className={styles.subItem}
+                  data-theme="azul"
+                >
+                  <NavLink to={item.subItem.rota} className={styles.menuLink}>
+                    {item.subItem.texto}
+                  </NavLink>
                 </li>
               </ul>
             )}
