e3d4b84409b3314615219f098ffe92b9b9a8d8b6
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-07-05 21:06:17 -0300

    feat(assets): cria arquivo para exportar as  imagens dos projetos e faz upload

---
+++ src/assets/projects/index.js
@@ -0,0 +1,20 @@
+import fokusCover from "./covers/fokus.png";
+import CodeConnectCover from "./covers/codeconnect.png";
+import memotecaCover from "./covers/memoteca.png";
+import AluraBooksCover from "./covers/alurabooks.png";
+
+import fokus01 from "./gallery/fokus/01.webp.png";
+import fokus02 from "./gallery/fokus/02.webp.png";
+import CodeConnect01 from "./gallery/codeConnect/01.webp.png";
+import CodeConnect02 from "./gallery/codeConnect/02.webp.png";
+
+export {
+  fokusCover,
+  CodeConnectCover,
+  memotecaCover,
+  AluraBooksCover,
+  fokus02,
+  fokus01,
+  CodeConnect01,
+  CodeConnect02,
+};
