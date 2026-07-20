97221fddfd35ea0530ecbf68d86a051281421a22
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-07-20 09:02:06 -0300

    fix(carousel): corrige sombras

--- src/pages/landingPage/sections/portifolio/gallery/carousel/thumbsPreview/thumb/index.jsx
+++ src/pages/landingPage/sections/portifolio/gallery/carousel/thumbsPreview/thumb/index.jsx
@@ -1,7 +1,11 @@
 import styles from "./thumbs.module.css";
 export const Thumb = ({ onClick, projeto }) => {
   return (
-    <button onClick={onClick} aria-label={`Ir para o projeto ${projeto.title}`}>
+    <button
+      onClick={onClick}
+      aria-label={`Ir para o projeto ${projeto.title}`}
+      className={styles.thumbButton}
+    >
       <img
         src={projeto.images[0]}
         alt=""
