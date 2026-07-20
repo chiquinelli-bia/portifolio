c6991c7ceec80a5d58abf01101c0d6cd371ebbbb
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-07-20 08:48:10 -0300

    feat(ThumbsPreview): adiciona rotação automática das imagens
    - implementa autoplay da galeria
    - reinicia temporizador após interação do usuário
    - ajusta navegação circular das imagens

--- src/pages/landingPage/sections/portifolio/gallery/carousel/thumbsPreview/index.jsx
+++ src/pages/landingPage/sections/portifolio/gallery/carousel/thumbsPreview/index.jsx
@@ -1,14 +1,14 @@
 import styles from "./thumbsPreview.module.css";
 import { Thumb } from "./thumb";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import { useGalleryContext } from "@/contexts/galleryContext/galleryContext";
 
 export const ThumbsPreview = ({ projects, emblaApi }) => {
-  const { currentIndex } = useGalleryContext();
+  const { currentIndex, setCurrentImage, currentImage } = useGalleryContext();
 
-  const totalProjects = projects.length;
-  const previousIndex = (currentIndex - 1 + totalProjects) % totalProjects;
-  const nextIndex = (currentIndex + 1) % totalProjects;
+  const totalImages = projects[currentIndex].images.length;
+  const previousIndex = (currentIndex - 1 + totalImages) % totalImages;
+  const nextIndex = (currentIndex + 1) % totalImages;
   const previousProject = projects[previousIndex];
   const nextProject = projects[nextIndex];
 
@@ -19,6 +19,13 @@ export const ThumbsPreview = ({ projects, emblaApi }) => {
     },
     [emblaApi],
   );
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prev) => (prev + 1) % totalImages);
+    }, 3500);
+
+    return () => clearInterval(interval);
+  }, [setCurrentImage, totalImages, currentImage]);
 
   return (
     <div className={styles.thumbsContainer}>
