74108c7846efb4f4ebb1ebabb765b944d6af73f3
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-07-27 08:31:01 -0300

    feat(ProjectInfo): cria estrutura da seção de informações do projeto
    - adiciona título e descrição do projeto
    - estrutura layout principal da seção
    - integra dados do projeto ao GalleryContext

--- src/pages/landingPage/sections/portifolio/gallery/projectInfo/index.jsx
+++ src/pages/landingPage/sections/portifolio/gallery/projectInfo/index.jsx
@@ -1,3 +1,35 @@
+import { Heading } from "@/components/heading";
+import { useGalleryContext } from "@/contexts/galleryContext/galleryContext";
+import styles from "./projectInfo.module.css";
+
 export const Info = () => {
-  return <div style={{ flex: "1" }}>Info</div>;
+  const { projects, currentIndex } = useGalleryContext();
+  const currentProject = projects[currentIndex];
+
+  return (
+    <div className={styles.container}>
+      <Heading as="h3" variant="lg" className={styles.projectTitle}>
+        {currentProject.title}
+      </Heading>
+      <div className={styles.projectContent}>
+        <span className={styles.divider} />
+        <div className={styles.content}>
+          <p>{currentProject.description}</p>
+          <Heading as="h4" variant="md">
+            Funcionalidades
+          </Heading>
+          <ul className={styles.featureList}></ul>
+          <Heading as="h4" variant="md">
+            Desafio & Conquista
+          </Heading>
+          <ul className={styles.highlightList}></ul>
+          <Heading as="h4" variant="md">
+            Tecnologias Utilizadas
+          </Heading>
+          <ul className={styles.technologiesList}></ul>
+          <div className={styles.containerButton}></div>
+        </div>
+      </div>
+    </div>
+  );
 };
