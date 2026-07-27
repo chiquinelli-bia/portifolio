17f829ac44e4f502a2c76016ded90ff00f92ecb0
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-07-27 08:27:00 -0300

    feat(App): adiciona rota para projetos ao router

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "@/styles/global.css";
 import { LandingPage } from "./pages/landingPage";
 import { MainLayout } from "./pages/layouts/mainLayout";
+import { Projects } from "./pages/projects";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<LandingPage />} />
+          <Route path="/projetos" element={<Projects />} />
         </Route>
       </Routes>
     </BrowserRouter>
