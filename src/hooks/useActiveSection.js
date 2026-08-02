e26ed9e220bf7b3cc2ff0b23e0fe85cbcd5b8a30
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-08-01 21:49:11 -0300

    feat(useActiveSection): adiciona hook para controlar tema pela seção ativa -monitora e identifica as seções da página com Intersection Observer

---
+++ src/hooks/useActiveSection.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+export function useActiveSection() {
+  const [activeSection, setActiveSection] = useState("");
+
+  useEffect(() => {
+    const sections = document.querySelectorAll("section");
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveSection(entry.target.id);
+          }
+        });
+      },
+      {
+        threshold: 0.5,
+      },
+    );
+
+    sections.forEach((section) => {
+      observer.observe(section);
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return activeSection;
+}
