a1a113074f2fa5d63a2153d4f4c3802e33a883f0
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-08-08 14:48:18 -0300

    feat(quemSou): adiciona componentes da seção de introdução e estiliza - adiciona font-size global para paaragrafos

--- src/pages/landingPage/sections/quemSou/index.jsx
+++ src/pages/landingPage/sections/quemSou/index.jsx
@@ -1,6 +1,7 @@
 import { SectionHeader } from "@/components/sectionHeader";
 import styles from "./quemSou.module.css";
 import { sections } from "@/data/navThemes";
+import { Heading } from "@/components/heading";
 
 export const QuemSou = () => {
   return (
@@ -9,8 +10,25 @@ export const QuemSou = () => {
         <SectionHeader title={sections[2].title} brand="yellow" />
         <div className={styles.sectionDecoration} />
       </div>
-
-      <div className={styles.sectionContent}></div>
+      <div className={styles.sectionContent}>
+        <div className={styles.introduction}>
+          <Heading as="h3" variant="lg">
+            Prólogo Pessoal
+          </Heading>
+          <p>
+            Desde 2022, construo minha trajetória movida pela{" "}
+            <span className={styles.textoDestaque}>
+              curiosidade e pelo fascínio pela área.
+            </span>{" "}
+            Aprofundo conhecimentos para entregar com qualidade e precisão, vejo
+            cada desafio como uma oportunidade de explorar novos caminhos e
+            criar resultados{" "}
+            <span className={styles.textoDestaque}>
+              eficientes, inovadores e com propósito.
+            </span>
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
