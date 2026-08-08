21814c6c4ff42481f1ae1553440003b93a82cc29
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-08-08 14:09:30 -0300

    feat(quemSou): adiciona decoração visual à seção
    - Cria o container sectionDecoration, adiciona imagens decorativas via CSS e aplica estilos de posicionamento e dimensionamento.

--- src/pages/landingPage/sections/quemSou/index.jsx
+++ src/pages/landingPage/sections/quemSou/index.jsx
@@ -5,9 +5,12 @@ import { sections } from "@/data/navThemes";
 export const QuemSou = () => {
   return (
     <section id="quem-sou" className={styles.container}>
-      <div className={styles.containerCarousel}>
+      <div className={styles.sectionVisual}>
         <SectionHeader title={sections[2].title} brand="yellow" />
+        <div className={styles.sectionDecoration} />
       </div>
+
+      <div className={styles.sectionContent}></div>
     </section>
   );
 };
