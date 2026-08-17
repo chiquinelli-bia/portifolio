ed30a3d428f2d47ec4511573e0c6f36e049c67a3
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-08-17 08:51:16 -0300

    feat(Tabs): importa componente buttonTabs e adiciona display flex

--- src/pages/landingPage/sections/quemSou/tabs/index.jsx
+++ src/pages/landingPage/sections/quemSou/tabs/index.jsx
@@ -1,4 +1,27 @@
+import { useState } from "react";
 import styles from "./tabs.module.css";
+import { ButtonTab } from "./buttonTab";
 export const Tabs = () => {
-  return <div>Tabs</div>;
+  const [activeTab, setActiveTab] = useState("competencias");
+
+  return (
+    <div
+      role="tablist"
+      aria-label="Informações profissionais"
+      className={styles.tabs}
+    >
+      <ButtonTab
+        activeTab={activeTab}
+        aba="competencias"
+        setActiveTab={setActiveTab}
+        label="Competências"
+      />
+      <ButtonTab
+        activeTab={activeTab}
+        aba="valores"
+        setActiveTab={setActiveTab}
+        label="Valores e Missão"
+      />
+    </div>
+  );
 };
