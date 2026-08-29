4e9c9d23eacb60ae3949e4f404512f60fcaed801
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-08-29 11:21:34 -0300

    feat(competenciasTab): estrutura componente, adiciona Heading e paragrafos e estilos básicos

--- src/pages/landingPage/sections/quemSou/tabs/competenciasTab/index.jsx
+++ src/pages/landingPage/sections/quemSou/tabs/competenciasTab/index.jsx
@@ -1,4 +1,28 @@
 import styles from "./competencias.module.css";
+import { Heading } from "@/components/heading";
+
 export const Competencias = () => {
-  return <div>Tabs</div>;
+  return (
+    <div
+      role="tabpanel"
+      aria-labelledby="competencias-tab"
+      className={styles.container}
+    >
+      <div className={styles.containerHabilidades}>
+        <Heading as="h4" variant="sm">
+          Habilidades que utilizo no dia a dia
+        </Heading>
+      </div>
+      <div className={styles.containerDisponibilidades}>
+        <Heading as="h4" variant="sm">
+          Disponibilidade e contratos
+        </Heading>
+        <p>
+          Disponível de segunda a sexta, nos períodos da manhã, tarde e
+          parcialmente à noite, pronta para novos projetos com dedicação e
+          compromisso.
+        </p>
+      </div>
+    </div>
+  );
 };
