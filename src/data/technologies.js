c0d83e490b6d65a39ccf3af4fe5d1bb96d16e209
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-08-29 11:24:57 -0300

    feat(technologies): cria estrutura de dados para implementação do badgeProgress

---
+++ src/data/technologies.js
@@ -0,0 +1,61 @@
+import {
+  Html,
+  Css,
+  JavaScript,
+  React,
+  Redux,
+  // Vite,
+  // Axios,
+} from "@/assets/technologies/index.js";
+
+export const technologies = [
+  {
+    id: 1,
+    name: "HTML",
+    icon: Html,
+    percentage: 85,
+    category: "Front-end",
+  },
+  {
+    id: 2,
+    name: "CSS",
+    icon: Css,
+    percentage: 75,
+    category: "Front-end",
+  },
+  {
+    id: 3,
+    name: "JavaScript",
+    icon: JavaScript,
+    percentage: 70,
+    category: "Front-end",
+  },
+  {
+    id: 4,
+    name: "React",
+    icon: React,
+    percentage: 75,
+    category: "Front-end",
+  },
+  {
+    id: 5,
+    name: "Redux",
+    icon: Redux,
+    percentage: 30,
+    category: "Front-end",
+  },
+  //   {
+  //     id: 6,
+  //     name: "Axios",
+  //     icon: Axios,
+  //     percentage: 90,
+  //     category: "Biblioteca",
+  //   },
+  //   {
+  //     id: 7,
+  //     name: "Vite",
+  //     icon: Vite,
+  //     percentage: 70,
+  //     category: "Ferramenta",
+  //   },
+];
