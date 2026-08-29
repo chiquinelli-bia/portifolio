5793416303cc6ba4600cf26b96b8bef5f83afe5b
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-08-29 11:20:31 -0300

    feat(ValoresEMissaoTab): estrutura componente, adiciona Heading e estilos básicos

--- src/pages/landingPage/sections/quemSou/tabs/valoresEMissaoTab/index.jsx
+++ src/pages/landingPage/sections/quemSou/tabs/valoresEMissaoTab/index.jsx
@@ -1,4 +1,23 @@
 import styles from "./valoresEMissao.module.css";
+import { Heading } from "@/components/heading";
+
 export const ValoresEMissao = () => {
-  return <div>Tabs</div>;
+  return (
+    <div
+      role="tabpanel"
+      aria-labelledby="competencias-tab"
+      className={styles.container}
+    >
+      <div className={styles.containerValores}>
+        <Heading as="h4" variant="sm">
+          Valores Profissional
+        </Heading>
+      </div>
+      <div className={styles.containerMissao}>
+        <Heading as="h4" variant="sm">
+          Missão profissional
+        </Heading>
+      </div>
+    </div>
+  );
 };
